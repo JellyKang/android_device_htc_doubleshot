28e5ac7ba6a0ebb1f19c4b0c4709a08ef3a2fb23
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-03-26 07:54:47 -0500

    Update romver

--- kang.mk
+++ kang.mk
@@ -1,6 +1,6 @@
 # JellyKang
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kang.version=V1.2-Doubleshot-$(shell date +%y%m%d) \
+    ro.kang.version=V1.3-Doubleshot-$(shell date +%y%m%d) \
     ro.goo.developerid=xmcwildchild22 \
     ro.goo.board=doubleshot \
     ro.goo.version=$(shell date +%y%m%d) \
