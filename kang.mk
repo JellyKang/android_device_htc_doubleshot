2531c36605add2c347cc001f2cd52d15207415b3
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-04-01 17:41:46 -0500

    Update rom details

--- kang.mk
+++ kang.mk
@@ -1,10 +1,10 @@
 # JellyKang
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kang.version=V1.3-Doubleshot-$(shell date +%y%m%d) \
+    ro.kang.version=V1.4-Doubleshot-$(shell date +%y%m%d) \
     ro.goo.developerid=xmcwildchild22 \
     ro.goo.board=doubleshot \
     ro.goo.version=$(shell date +%y%m%d) \
-    ro.goo.rom=JellyKang-JB4.2-Doubleshot 
+    ro.goo.rom=JellyKang-Doubleshot 
 
 PRODUCT_PACKAGES += \
     LiquidPapers \
