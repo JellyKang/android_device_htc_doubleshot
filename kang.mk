1cc1439f580877f91dd7016c478d207a2716994d
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-04-19 00:50:08 -0500

    Update rom ver

--- kang.mk
+++ kang.mk
@@ -1,6 +1,6 @@
 # JellyKang
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kang.version=V1.5-Doubleshot-$(shell date +%y%m%d) \
+    ro.kang.version=V1.6-Doubleshot-$(shell date +%y%m%d) \
     ro.goo.developerid=xmcwildchild22 \
     ro.goo.board=doubleshot \
     ro.goo.version=$(shell date +%y%m%d) \
