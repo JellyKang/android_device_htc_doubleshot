147db3e25184f05d9a91dd19ad9758d466b981d4
Emmanuel Utomi <emmanuelutomi@gmail.com>
2013-05-25 12:38:41 -0500

    Add support for multiple repos

--- slim.mk
+++ slim.mk
@@ -5,20 +5,20 @@ $(call inherit-product-if-exists, vendor/kang/kang.mk)
 $(call inherit-product, device/htc/doubleshot/device_doubleshot.mk)
 
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := doubleshot
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_doubleshot BUILD_FINGERPRINT=tmous/htc_doubleshot/doubleshot:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
 
 # Device naming
 PRODUCT_DEVICE := doubleshot
-PRODUCT_NAME := cm_doubleshot
+PRODUCT_NAME := slim_doubleshot
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := MyTouch 4G Slide
 PRODUCT_MANUFACTURER := HTC
